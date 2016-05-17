16600d8b7e21f845e070e425142874c02060d809
datsuns <the.skeleton7@gmail.com>
2016-05-17 22:10:09 +0900

    add basic interfaces

--- scmock.h
+++ scmock.h
@@ -1,10 +1,49 @@
 #ifndef SIMPLE_C_MOCK_H_INCLUDED
 #define SIMPLE_C_MOCK_H_INCLUDED
 
-// [ ]mock interface declearation
-// [ ]set return value
+//--------------------------------
+// basic definition
+#define SCMOCK_PARAM(name, n)                  _scmock_param_##name##_##n
+#define SCMOCK_DECLARE_PARAM(name, type, n)    type SCMOCK_PARAM(name, n)
+#define SCMOCK_RET(name)                        _scmock_ret_##name
+#define SCMOCK_SET_RET(name, v)                 SCMOCK_RET(name) = v
 
-#define SCMOCK_DECLARE( name ) void name( void ){}
+//--------------------------------
+// no return value mocks
+#define SCMOCK_DECLARE(name)  \
+  void name( void ){}
+
+#define SCMOCK_DECLARE_P1(name, type0)    \
+  SCMOCK_DECLARE_PARAM(name, type0, 0);   \
+  void name(type0 p){                     \
+    SCMOCK_PARAM(name, 0) = p;            \
+  }
+
+#define SCMOCK_DECLARE_P2(name, type0, type1)     \
+  SCMOCK_DECLARE_PARAM(name, type0, 0);    \
+  SCMOCK_DECLARE_PARAM(name, type1, 1);    \
+  void name(type0 p0, type1 p1){           \
+    SCMOCK_PARAM(name, 0) = p0;            \
+    SCMOCK_PARAM(name, 1) = p1;            \
+  }
+
+//--------------------------------
+// has return value
+#define SCMOCK_DECLARE_RET_VAL(ret_type, name)  ret_type SCMOCK_RET(name)
+
+#define SCMOCK_DECLARE_RET(ret_type, name)  \
+  SCMOCK_DECLARE_RET_VAL(ret_type, name);   \
+  ret_type name( void ){                    \
+    return SCMOCK_RET(name);                \
+  }
+
+#define SCMOCK_DECLARE_RET_P1(ret_type, name, type0)  \
+  SCMOCK_DECLARE_PARAM(name, type0, 0);     \
+  SCMOCK_DECLARE_RET_VAL(ret_type, name);   \
+  ret_type name(type0 p0){                  \
+    SCMOCK_PARAM(name, 0) = p0;             \
+    return SCMOCK_RET(name);                \
+  }
 
 
 #endif
--- test/test.c
+++ test/test.c
@@ -1,11 +1,32 @@
 #include <assert.h>
 #include <stdio.h>
+#include <string.h>
 
 #include <scmock.h>
 
 SCMOCK_DECLARE( hello )
+SCMOCK_DECLARE_P1( hello2, char* )
+SCMOCK_DECLARE_RET( char, hello3 )
+SCMOCK_DECLARE_P2( hello4, char*, int )
+SCMOCK_DECLARE_RET_P1( char, hello5, int )
 
 int main(void){
   hello();
+
+  hello2("hello");
+  assert( strcmp(SCMOCK_PARAM(hello2, 0), "hello") == 0 );
+
+  SCMOCK_SET_RET(hello3, 1);
+  assert(hello3() == 1);
+  SCMOCK_SET_RET(hello3, 2);
+  assert(hello3() == 2);
+
+  hello4("abc", 10);
+  assert( strcmp(SCMOCK_PARAM(hello4, 0), "abc") == 0 );
+  assert( SCMOCK_PARAM(hello4, 1) == 10 );
+
+  SCMOCK_SET_RET(hello5, 1);
+  assert(hello5(10) == 1);
+  assert( SCMOCK_PARAM(hello5, 0) == 10 );
   return 0;
 }
